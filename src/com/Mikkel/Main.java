c5cf9d8309a433f67a47015e2810155fb38e3d16
mikkelvase <Mikk864n@stud.kea.dkgit config --global user.email mikk864n@stud.eka.dk>
2020-05-14 19:54:35 +0200

    Mikkels nye commit

--- src/com/Mikkel/Main.java
+++ src/com/Mikkel/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("Hej2");
         System.out.println("Hej fra mikkel :)");
         System.out.println("Hej fra mikkel :)");
-        System.out.println("Hej fra mikkel :)");
+        // Hello
     }
 }
