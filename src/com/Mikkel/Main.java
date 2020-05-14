ddb88f18b1289fb0a1c9fd9bb6df4f2c06aeb242
simonklejnstrup <63997120+simonklejnstrup@users.noreply.github.com>
2020-05-14 19:44:07 +0200

    Simons test

--- src/com/Mikkel/Main.java
+++ src/com/Mikkel/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hej");
         System.out.println("Hej2");
+        System.out.println("Hej Fra Simon");
     }
 }
