bad6df4e441692421d5aa97998058865f11b1ef6
Blyatman666 <43138836+Blyatman666@users.noreply.github.com>
2020-05-18 06:01:13 +0200

    En ide til, hvordan disciplinerne kunne tilføjes til en svømmer. Og Økonomi-klasse slettet.

--- src/Controller/Svømmer.java
+++ src/Controller/Svømmer.java
@@ -2,6 +2,7 @@ package Controller;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
 
 public class Svømmer {
     private String navn;
@@ -11,15 +12,17 @@ public class Svømmer {
     private String medlemskabsType; //KonkurrenceSvømmer-Klasse istedet for String
     private boolean inRestance;
     private int balance;
+    private ArrayList<String> disciplin = new ArrayList<>();
 
 
-    public Svømmer(String navn, LocalDate fødselsdag, String adresse, String aktivitetsform, String medlemskabsType) {
+    public Svømmer(String navn, LocalDate fødselsdag, String adresse, String aktivitetsform, String medlemskabsType, disciplin) {
         this.navn = navn;
         this.fødselsdag = fødselsdag;
         this.adresse = adresse;
         this.aktivitetsform = aktivitetsform;
         this.medlemskabsType = medlemskabsType;
         balance = 0;
+        this.disciplin = disciplin;
     }
 
     //Overload
@@ -52,6 +55,10 @@ public class Svømmer {
     public boolean isInRestance() {
         return balance < 0;
 
+    }
+
+    public void tilføjDisciplin(String tilføjDisciplin){
+        disciplin.add(tilføjDisciplin);
     }
 
     //Usikker på om vi skal bruge  den her metode..?
